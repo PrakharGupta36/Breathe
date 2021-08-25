0d30b1a645b803051fdccbefd887ddf9999bac5f
prakhar_36 <prakhargupta3680@gmail.com>
2021-08-25 13:49:35 +0530

    PWA fixed

--- service-worker.js
+++ service-worker.js
@@ -6,23 +6,3 @@ workbox.routing.registerRoute(
   ({ request }) => request.destination === "image",
   new workbox.strategies.NetworkFirst()
 );
-
-let defferedPrompt;
-const addbtn = document.getElementById("pwa");
-
-window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  defferedPrompt = event;
-  addbtn.style.display = "block";
-});
-
-addbtn.addEventListener("click", (event) => {
-  defferedPrompt.prompt();
-
-  defferedPrompt.userChoice.then((choice) => {
-    if (choice.outcome === "accepted") {
-      console.log("user accepted the prompt");
-    }
-    defferedPrompt = null;
-  });
-});
