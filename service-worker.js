c3aca60b662d4332b0c36922f9ea707423bf7b02
prakhar_36 <prakhargupta3680@gmail.com>
2021-08-25 13:43:24 +0530

    PWA fixed

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,28 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js"
-),
-  workbox.routing.registerRoute(
-    ({ request: a }) => "image" === a.destination,
-    new workbox.strategies.NetworkFirst()
-  );
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.NetworkFirst()
+);
+
 let defferedPrompt;
 const addbtn = document.getElementById("pwa");
-window.addEventListener("beforeinstallprompt", (a) => {
-  a.preventDefault(), (defferedPrompt = a), (addbtn.style.display = "block");
-}),
-  addbtn.addEventListener("click", () => {
-    defferedPrompt.prompt(),
-      defferedPrompt.userChoice.then((a) => {
-        "accepted" === a.outcome && console.log("user accepted the prompt"),
-          (defferedPrompt = null);
-      });
-  }),
-  window.addEventListener("beforeinstallprompt", (a) => {
-    a.preventDefault(),
-      (deferredPrompt = a),
-      showInstallPromotion(),
-      console.log(`'beforeinstallprompt' event was fired.`);
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  defferedPrompt = event;
+  addbtn.style.display = "block";
+});
+
+addbtn.addEventListener("click", (event) => {
+  defferedPrompt.prompt();
+
+  defferedPrompt.userChoice.then((choice) => {
+    if (choice.outcome === "accepted") {
+      console.log("user accepted the prompt");
+    }
+    defferedPrompt = null;
   });
+});
